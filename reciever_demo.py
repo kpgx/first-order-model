2ed9c4eefa2de7702de6db2bdc5cabe468e2b9fd
kasun_theta3 <kasung@theta3>
2021-10-14 15:07:21 +0800

    added decoupled scripts to send and recieve

--- reciever_demo.py
+++ reciever_demo.py
@@ -24,7 +24,7 @@ import _pickle as cPickle
 if sys.version_info[0] < 3:
     raise Exception("You must use Python 3 or higher. Recommended version is Python 3.7")
 
-def load_checkpoints(config_path, checkpoint_path, cpu=False):
+def load_generator(config_path, checkpoint_path, cpu=False):
 
     with open(config_path) as f:
         config = yaml.load(f)
@@ -34,27 +34,19 @@ def load_checkpoints(config_path, checkpoint_path, cpu=False):
     if not cpu:
         generator.cuda()
 
-    kp_detector = KPDetector(**config['model_params']['kp_detector_params'],
-                             **config['model_params']['common_params'])
-    if not cpu:
-        kp_detector.cuda()
-    
     if cpu:
         checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
     else:
         checkpoint = torch.load(checkpoint_path)
  
     generator.load_state_dict(checkpoint['generator'])
-    kp_detector.load_state_dict(checkpoint['kp_detector'])
     
     if not cpu:
         generator = DataParallelWithCallback(generator)
-        kp_detector = DataParallelWithCallback(kp_detector)
 
     generator.eval()
-    kp_detector.eval()
     
-    return generator, kp_detector
+    return generator
 
 
 def make_animation(source_image, key_points, generator, relative=True, adapt_movement_scale=True, cpu=False):
@@ -117,19 +109,11 @@ if __name__ == "__main__":
     parser.add_argument("--config", required=True, help="path to config")
     parser.add_argument("--checkpoint", default='vox-cpk.pth.tar', help="path to checkpoint to restore")
 
-    #parser.add_argument("--source_image", default='sup-mat/source.png', help="path to source image")
-    #parser.add_argument("--driving_video", default='sup-mat/source.png', help="path to driving video")
     parser.add_argument("--result_video", default='result.mp4', help="path to output")
  
     parser.add_argument("--relative", dest="relative", action="store_true", help="use relative or absolute keypoint coordinates")
     parser.add_argument("--adapt_scale", dest="adapt_scale", action="store_true", help="adapt movement scale based on convex hull of keypoints")
 
-    #parser.add_argument("--find_best_frame", dest="find_best_frame", action="store_true", 
-    #                    help="Generate from the frame that is the most alligned with source. (Only for faces, requires face_aligment lib)")
-
-    #parser.add_argument("--best_frame", dest="best_frame", type=int, default=None,  
-    #                    help="Set frame to start from.")
- 
     parser.add_argument("--cpu", dest="cpu", action="store_true", help="cpu mode.")
     parser.add_argument("--in_file_name", default="video.pkl.pbz2", help="sender output file")
 
@@ -138,29 +122,7 @@ if __name__ == "__main__":
 
     opt = parser.parse_args()
 
-
-    #reader = imageio.get_reader(opt.driving_video)
-    #fps = reader.get_meta_data()['fps']
-    #driving_video = []
-    #try:
-    #    for im in reader:
-    #        driving_video.append(im)
-    #except RuntimeError:
-    #    pass
-    #reader.close()
-
-    #driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
-    generator, _ = load_checkpoints(config_path=opt.config, checkpoint_path=opt.checkpoint, cpu=opt.cpu)
-
-    #if opt.find_best_frame or opt.best_frame is not None:
-    #    i = opt.best_frame if opt.best_frame is not None else find_best_frame(source_image, driving_video, cpu=opt.cpu)
-    #    print ("Best frame: " + str(i))
-    #    driving_forward = driving_video[i:]
-    #    driving_backward = driving_video[:(i+1)][::-1]
-    #    predictions_forward = make_animation(source_image, driving_forward, generator, kp_detector, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu)
-    #    predictions_backward = make_animation(source_image, driving_backward, generator, kp_detector, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu)
-    #    predictions = predictions_backward[::-1] + predictions_forward[1:]
-    #else:
+    generator = load_generator(config_path=opt.config, checkpoint_path=opt.checkpoint, cpu=opt.cpu)
 
     in_file = load_compressed_pickle(opt.in_file_name)
 
@@ -170,7 +132,6 @@ if __name__ == "__main__":
 
     key_points = in_file["key_points"]
 
-    #print("key_points", key_points)
     predictions = make_animation(source_image, key_points, generator, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu)
     imageio.mimsave(opt.result_video, [img_as_ubyte(frame) for frame in predictions], fps=fps)
 
--- sender_demo.py
+++ sender_demo.py
@@ -24,15 +24,10 @@ import _pickle as cPickle
 if sys.version_info[0] < 3:
     raise Exception("You must use Python 3 or higher. Recommended version is Python 3.7")
 
-def load_checkpoints(config_path, checkpoint_path, cpu=False):
+def load_kp_detector(config_path, checkpoint_path, cpu=False):
 
     with open(config_path) as f:
         config = yaml.load(f)
-
-    #generator = OcclusionAwareGenerator(**config['model_params']['generator_params'],
-    #                                    **config['model_params']['common_params'])
-    #if not cpu:
-    #    generator.cuda()
 
     kp_detector = KPDetector(**config['model_params']['kp_detector_params'],
                              **config['model_params']['common_params'])
@@ -44,17 +39,14 @@ def load_checkpoints(config_path, checkpoint_path, cpu=False):
     else:
         checkpoint = torch.load(checkpoint_path)
  
-    #generator.load_state_dict(checkpoint['generator'])
     kp_detector.load_state_dict(checkpoint['kp_detector'])
     
     if not cpu:
-    #    generator = DataParallelWithCallback(generator)
         kp_detector = DataParallelWithCallback(kp_detector)
 
-    #generator.eval()
     kp_detector.eval()
     
-    return None, kp_detector
+    return kp_detector
 
 
 def get_key_points(source_image, driving_video, kp_detector, relative=True, adapt_movement_scale=True, cpu=False):
@@ -117,19 +109,12 @@ if __name__ == "__main__":
     parser.add_argument("--config", required=True, help="path to config")
     parser.add_argument("--checkpoint", default='vox-cpk.pth.tar', help="path to checkpoint to restore")
 
-    #parser.add_argument("--source_image", default='sup-mat/source.png', help="path to source image")
     parser.add_argument("--driving_video", default='sup-mat/source.png', help="path to driving video")
     parser.add_argument("--result_video", default='result.mp4', help="path to output")
  
     parser.add_argument("--relative", dest="relative", action="store_true", help="use relative or absolute keypoint coordinates")
     parser.add_argument("--adapt_scale", dest="adapt_scale", action="store_true", help="adapt movement scale based on convex hull of keypoints")
 
-    #parser.add_argument("--find_best_frame", dest="find_best_frame", action="store_true", 
-    #                    help="Generate from the frame that is the most alligned with source. (Only for faces, requires face_aligment lib)")
-
-    #parser.add_argument("--best_frame", dest="best_frame", type=int, default=None,  
-    #                    help="Set frame to start from.")
- 
     parser.add_argument("--cpu", dest="cpu", action="store_true", help="cpu mode.")
  
 
@@ -138,10 +123,8 @@ if __name__ == "__main__":
 
     opt = parser.parse_args()
 
-    #source_image = imageio.imread(opt.source_image)
     reader = imageio.get_reader(opt.driving_video)
     fps = reader.get_meta_data()['fps']
-    print("FPS", fps)
     driving_video = []
     try:
         for im in reader:
@@ -152,22 +135,9 @@ if __name__ == "__main__":
 
     source_image = resize(driving_video[0], (256, 256))[..., :3]
     driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
-    _, kp_detector = load_checkpoints(config_path=opt.config, checkpoint_path=opt.checkpoint, cpu=opt.cpu)
+    kp_detector = load_kp_detector(config_path=opt.config, checkpoint_path=opt.checkpoint, cpu=opt.cpu)
 
-    #if opt.find_best_frame or opt.best_frame is not None:
-    #    i = opt.best_frame if opt.best_frame is not None else find_best_frame(source_image, driving_video, cpu=opt.cpu)
-    #    print ("Best frame: " + str(i))
-    #    driving_forward = driving_video[i:]
-    #    driving_backward = driving_video[:(i+1)][::-1]
-    #    predictions_forward = make_animation(source_image, driving_forward, generator, kp_detector, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu)
-    #    predictions_backward = make_animation(source_image, driving_backward, generator, kp_detector, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu)
-    #    predictions = predictions_backward[::-1] + predictions_forward[1:]
-    #else:
     key_points = get_key_points(source_image, driving_video, kp_detector, relative=opt.relative, adapt_movement_scale=opt.adapt_scale, cpu=opt.cpu)
     out_data = {"src_img": source_image, "fps": fps, "key_points": key_points}
-    #print("key_points", key_points)
     write_compressed_pickle(out_data, 'video.pkl')
-    #with open("kp.pkl", 'wb') as out_file:
-    #    pickle.dump(key_points, out_file, pickle.HIGHEST_PROTOCOL)
-    #imageio.mimsave(opt.result_video, [img_as_ubyte(frame) for frame in predictions], fps=fps)
 
