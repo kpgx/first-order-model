ce6b8856f23ccd2a6d9da88af4abb71bfb6383b0
kasun <kasung@smu.edu.sg>
2022-02-23 13:43:01 +0800

    added script to exectue command in loop

--- execute_in_loop.py
+++ execute_in_loop.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
-command = ["python3", "sender.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4"]
+command = ["python3", "onnx_sender.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4"]
+#command = ["python3", "sender.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4"]
 #command_2 =["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "working/driving/x10_000000.mp4", "-c:v", "h264", "-preset", "medium", "-bf", "0", "out_x10_0000000.mp4"]
 #command_3 =["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "working/driving/x10_000000.mp4", "-c:v", "hevc", "-preset", "medium", "-x265-params", "bframes=0", "out_x10_0000000.mp4"]
 #command = ["python3", "prune_resnet18_cifar10.py", "--mode", "test", "--round"]                                                          
