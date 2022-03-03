c64391080eef2d8a1ebf0f9a962c3fe714e59c9f
kasun <kasung@smu.edu.sg>
2022-03-03 14:47:49 +0800

    updated the command + sleep time

--- execute_in_loop.py
+++ execute_in_loop.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
-command = ["python3", "onnx_sender.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4"]
+command = ["python3", "onnx_sender_cpu.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4", "--cpu"]
+#command = ["python3", "sender.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4", "--cpu"]
 #command = ["python3", "sender.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4"]
 #command_2 =["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "working/driving/x10_000000.mp4", "-c:v", "h264", "-preset", "medium", "-bf", "0", "out_x10_0000000.mp4"]
 #command_3 =["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "working/driving/x10_000000.mp4", "-c:v", "hevc", "-preset", "medium", "-x265-params", "bframes=0", "out_x10_0000000.mp4"]
@@ -10,15 +11,15 @@ log_file_name = "log.csv"
 
 start_round = 0
 end_round = 10
-sleep_time = 10
+sleep_time = 100
 
 def write_log_line(file_name, log_line):
     with open(file_name, 'a') as f:
         f.write(log_line + '\n')
 
+log_line = "ite,type,start,end"
+write_log_line(log_file_name, log_line)
 for cur_round in range(start_round, end_round):
-    log_line = "ite,type,start_ts,end_ts"
-    write_log_line(log_file_name, log_line)
        # start = (time.time())
        # subprocess.run(command)
        # end = (time.time())
