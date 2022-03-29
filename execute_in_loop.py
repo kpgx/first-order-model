7b62cbdc4b08b43d41cc6c1a24d6599fc506c2f2
kasun <kasung@smu.edu.sg>
2022-03-29 14:27:13 +0800

    updated

--- execute_in_loop.py
+++ execute_in_loop.py
@@ -1,7 +1,8 @@
 import subprocess
 import time
-command = ["python3", "onnx_sender_cpu.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4", "--cpu"]
-#command = ["python3", "sender.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4", "--cpu"]
+#command = ["python3", "onnx_sender_new.py", "--config", "config/bair-256.yaml", "--checkpoint", "int8_kpd.onnx", "--driving_video", "h264_long.mp4", "--run_time", "gpu"]
+#command = ["python3", "onnx_sender_new.py", "--config", "config/bair-256.yaml", "--checkpoint", "fp32_kpd.onnx", "--driving_video", "h264_long.mp4", "--run_time", "cpu"]
+command = ["python3", "sender.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4", "--cpu"]
 #command = ["python3", "sender.py", "--config", "config/bair-256.yaml", "--checkpoint", "checkpoints/bair-cpk.pth.tar", "--driving_video", "h264_long.mp4"]
 #command_2 =["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "working/driving/x10_000000.mp4", "-c:v", "h264", "-preset", "medium", "-bf", "0", "out_x10_0000000.mp4"]
 #command_3 =["ffmpeg","-hide_banner", "-loglevel", "error", "-i", "working/driving/x10_000000.mp4", "-c:v", "hevc", "-preset", "medium", "-x265-params", "bframes=0", "out_x10_0000000.mp4"]
