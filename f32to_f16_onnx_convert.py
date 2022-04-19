2b87789e21e18babf35413a0d150862cb68fd8f9
kasun <kasun.com>
2022-04-19 14:33:52 +0800

    refactor f32 to f16 converer

--- f32to_f16_onnx_convert.py
+++ f32to_f16_onnx_convert.py
@@ -2,8 +2,8 @@ import onnxmltools
 
 from onnxmltools.utils.float16_converter import convert_float_to_float16
 
-input_onnx_model = "fp32_kpd.onnx"
-output_onnx_model = "fp16_kpd.onnx"
+input_onnx_model = "conv3_fp32_kpd.onnx"
+output_onnx_model = "conv3_fp16_kpd.onnx"
 
 onnx_model = onnxmltools.utils.load_model(input_onnx_model)
 onnx_model = convert_float_to_float16(onnx_model)
