284b48a1497924367b15cab9a140123381ab865a
kasuntx2 <kasun@tx2>
2022-05-09 15:15:47 +0800

    script added to execute reconstruct h264 videos from bulk kp.npy files

--- execute_reconstruct_from_kp_for_all_files_in_dir.py
+++ execute_reconstruct_from_kp_for_all_files_in_dir.py
@@ -4,22 +4,24 @@ from os import listdir
 from os.path import isfile, join
 
 COMMAND = ["python3", "reciever.py",]
-DIR = "working"
+DIR = "mgif-test"
+FOM_DIR = join(DIR, "fom_mp4")
+src_file_ext = 'gif'
 
 
-def get_src_file_list_from_dir(mypath):
+def get_src_file_list_from_dir(mypath, ext):
     onlyfiles = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
-    only_src_files = [f for f in onlyfiles if f.lower().endswith('.mp4')]
+    only_src_files = [f for f in onlyfiles if f.lower().endswith(ext)]
     return only_src_files
 
 
-file_list = get_src_file_list_from_dir(DIR)
+file_list = get_src_file_list_from_dir(DIR, src_file_ext)
 
 for file_name in file_list:
-    result_video = file_name+"_fom"
-    in_kp_file = file_name+".kp"
+    result_video = join(FOM_DIR, file_name+"_fom.mp4")
+    in_kp_file = file_name+".kp.npy"
     in_img_file = file_name+".jpeg"
     current_command = COMMAND + ["--result_video", result_video, '--kp_file', in_kp_file, '--src_img', in_img_file]
     print(current_command)
-    # subprocess.run(current_command)
+    subprocess.run(current_command)
 
