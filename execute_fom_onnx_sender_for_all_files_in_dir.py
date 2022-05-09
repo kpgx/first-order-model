e0405c28fadd997a1ece80e2ad94f56100d7ef41
kasuntx2 <kasun@tx2>
2022-05-09 15:14:39 +0800

    script added to execute onnx fom conversion to bulk of files

---
+++ execute_fom_onnx_sender_for_all_files_in_dir.py
@@ -0,0 +1,24 @@
+import subprocess
+import time
+from os import listdir
+from os.path import isfile, join
+
+COMMAND = ["python3", "onnx_sender.py",]
+DIR = "mgif-test"
+
+
+def get_file_list_from_dir(mypath):
+    onlyfiles = [join(mypath, f) for f in listdir(mypath) if isfile(join(mypath, f))]
+    return onlyfiles
+
+
+file_list = get_file_list_from_dir(DIR)
+
+for file_name in file_list:
+    driving_video = file_name
+    out_kp_file = file_name+".kp"
+    out_img_file = file_name+".jpeg"
+    current_command = COMMAND + ["--driving_video", driving_video, '--out_kp_file', out_kp_file, '--out_img_file', out_img_file]
+    print(current_command)
+    subprocess.run(current_command)
+
