724c62a43631854942e7400f8b31676828c244c8
kasun <kasun.com>
2022-06-09 16:33:18 +0800

    updates from mac

--- parse_power_logs/process_h265_logs.py
+++ parse_power_logs/process_h265_logs.py
@@ -3,10 +3,12 @@ import pandas as pd
 import csv
 
 
-data_folder="mgif/h265"
+data_folder="/Users/larcuser/Projects/first-order-model/parse_power_logs/logs/vox-eval-100/h265"
 fom_file = data_folder + "/h265.csv"
 power_file = data_folder+"/jtop.csv"
 out_csv_file = data_folder+"/out.csv"
+
+TIMES = 1
 
 fom = pd.read_csv(fom_file, header=None)
 power = pd.read_csv(power_file)
@@ -14,14 +16,18 @@ power = pd.read_csv(power_file)
 power_readings_per_file = []
 for i, row in fom.iterrows():
     file_name, wait_start, wait_end, start, end, src_size, dest_size = row[0], row[1], row[2], row[3], row[4], row[5], row[6]
+    print(src_size)
     idle_power_readings = power.query("{} < ts < {}".format(wait_start+1, wait_end-1))
     # chosen_idle_power_reading = idle_power_readings["total power cur"][0]
     power_readings = power.query("{} < ts < {}".format(start+1, end-1))
     total_power_readings = []
     for i, reading in power_readings.iterrows():
         total_power_readings.append(reading["total power cur"])
-    avg_power_reading = sum(total_power_readings)/len(total_power_readings)/1000
-
+    try:
+        avg_power_reading = sum(total_power_readings)/len(total_power_readings)/1000
+    except ZeroDivisionError:
+        print("No enogh readings [{}]".format(file_name))
+        continue
     total_idle_power_readings = []
     for i, reading in idle_power_readings.iterrows():
         total_idle_power_readings.append(reading["total power cur"])
@@ -32,7 +38,7 @@ for i, row in fom.iterrows():
     avg_conversion_power = avg_power_reading-avg_idle_power_reading
 
     total_energy = avg_conversion_power * duration
-    current_data = {"file":file_name, "start":start, "end":end,"src_file_size":src_size,"h265_file_size":dest_size, "duration(sec)":duration/10,"avg_total_idle_power_reading":avg_idle_power_reading, "avg_total_processing_power_reading(W)":avg_power_reading,"avg_conversion_power(W)":avg_conversion_power, "total_energy(J)":total_energy/10}
+    current_data = {"file":file_name, "start":start, "end":end,"src_file_size":src_size,"h265_file_size":dest_size, "duration(sec)":duration/TIMES,"avg_total_idle_power_reading":avg_idle_power_reading, "avg_total_processing_power_reading(W)":avg_power_reading,"avg_conversion_power(W)":avg_conversion_power, "total_energy(J)":total_energy/TIMES}
 
     power_readings_per_file.append(current_data)
 
